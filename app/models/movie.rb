eb09fcaf2d089963392addd39e76e7ce3a80cc5a
Alonzo Rice <96135977+ricealonzo@users.noreply.github.com>
2024-08-29 15:30:23 +0000

    2

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -13,4 +13,12 @@
 #  director_id :integer
 #
 class Movie < ApplicationRecord
+  def the_director
+    
+    directors_id = self.director_id
+    matching_directors = Director.where({:id => directors_id})
+    the_directors = matching_directors.at(0)
+    
+  return the_directors
+  end
 end
